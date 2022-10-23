ad9d14dea913057b710a897e18753daf3fce2be5
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2022-10-22 22:01:33 -0700

    add safety pin (not working yet... I think the pin numbers are by the IC not the board, even for DIO

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,29 @@ VescUart VESC;
 uint16_t UPDATE_PERIOD_MS = 10;
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire1);
 
-const float current_limit = 10.0;
-const float current_gain = 0.5;
+const float current_limit = 50.0;
+const float current_gain = 3.0;
+
+const int safety_pin = 5;
+
+bool tag_in_place() {
+  return digitalRead(safety_pin) == HIGH;
+}
 
 void setup(void)
 {
-  // PIO configuration
+  // pin configuration
+
+  pinMode(safety_pin, INPUT_PULLDOWN);
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
 
   // StemmaQT / Quuic Connector Pin Selection
   Wire1.setSDA(2);
   Wire1.setSCL(3);
 
   // Serial Port Configuration for VESC
+  // TODO: confirm I don't need to do this
   // Serial1.setRX(3);
   // Serial1.setTX(2);
 
@@ -32,40 +43,63 @@ void setup(void)
   VESC.setSerialPort(&Serial1);
  
   // Trap Failed Configuration
-  if (!bno.begin())
+  while (!bno.begin())
   {
-    while (1) {
-      Serial.println("No BNO055 detected");
-      delay(1000);
-    }
+    Serial.println("No BNO055 detected");
+    delay(1000);
   }
 
   Serial.println("Setup Successful!");
+  delay(1000); // see if waiting for the VESC to come up helps
+
+  while (tag_in_place()) {
+    Serial.println("unplug safety tag");
+    delay(1000);
+  }
+  while (!tag_in_place()) {
+    Serial.println("replace safety tag");
+    delay(1000);
+  }
+  
 }
 
 void printOrientation(sensors_event_t *event)
 {
-  Serial.print("Orientation: ");
   Serial.print("X: ");
   Serial.print(event->orientation.x);
   Serial.print(", Y: ");
   Serial.print(event->orientation.y);
   Serial.print(", Z: ");
   Serial.print(event->orientation.z);
-  Serial.println();
 }
 
 void printCurrent(const float current)
 {
-  Serial.print("Current: ");
+  Serial.print(", Current: ");
   Serial.print(current);
-  Serial.println("A");
+}
+
+void printVescInfo()
+{
+  if ( VESC.getVescValues() ) {
+    Serial.print(", RPM: ");
+    Serial.print(VESC.data.rpm);
+    Serial.print(", Batt V: ");
+    Serial.print(VESC.data.inpVoltage);
+  }
 }
 
 void loop(void)
 {
-  //
   unsigned long tStart = micros();
+
+  // if the tags not in place, stop.
+  while (!tag_in_place()) {
+    VESC.setCurrent(0.0f);
+    Serial.println("replace safety tag");
+    delay(10);
+  }
+
   sensors_event_t orientationData;
   bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
   // 
@@ -86,11 +120,13 @@ void loop(void)
   if (max(abs(orientationData.orientation.y), abs(orientationData.orientation.z)) > 30)
   {
     current = 0;
-    Serial.println("Ahhh!");
   }
 
   printCurrent(current); // debug print out
+  printVescInfo();
   VESC.setCurrent(current);
+
+  Serial.println();
 
   // wait for the next cycle
   while ((micros() - tStart) < (UPDATE_PERIOD_MS * 1000)) {}
